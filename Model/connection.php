f7e640a8bc8be82f48006d19c29d9b739a50968a
Juan Aristizabal <juanaristizaballlano@fernandosor.edu.co>
2023-11-19 22:53:26 +0100

    creation model connection.php

---
+++ Model/connection.php
@@ -0,0 +1,21 @@
+<?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+define('DB_HOST', 'mysql-juanjokeylab.alwaysdata.net'); // Remplacez par votre hôte MySQL
+define('DB_USER', '332332_live'); // Remplacez par votre nom d'utilisateur MySQL
+define('DB_PASS', 'Unrealtournament2004!'); // Remplacez par votre mot de passe MySQL
+define('DB_NAME', 'juanjokeylab_live'); // Remplacez par le nom de votre base de données
+define('DB_URL', 'jdbc:mariadb://mysql-juanjokeylab.alwaysdata.net:3306/juanjokeylab_live');
+
+
+// // Crea una conexión
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+  die("La conexión falló: " . $conn->connect_error);
+}
+
+// echo "Conexión exitosa";
